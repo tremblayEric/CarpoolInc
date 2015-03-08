d1263d4915f4ba775da794a7b2eeb8794665c4c8
eric tremblay <tremblay_eric@live.ca>
2015-03-08 16:49:46 -0400

    premier commit

---
+++ app/src/main/java/mgl7130/tiroir/Reservation.java
@@ -0,0 +1,21 @@
+package mgl7130.tiroir;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by Bart on 2015-03-07.
+ */
+public class Reservation extends Fragment{
+View rootview;
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        rootview = inflater.inflate(R.layout.reservation_layout,container,false);
+        return rootview;
+    }
+}
