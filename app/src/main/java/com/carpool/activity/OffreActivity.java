5a37966ceb2fa5c3e6d47b4394948630cd284bd7
Gaëlle Claude <oceane3088@gmail.com>
2015-03-15 21:53:48 -0400

    Interface d'offre de covoiturage. Il ne s'agit que du design

--- app/src/main/java/com/carpool/activity/OffreActivity.java
+++ app/src/main/java/com/carpool/activity/OffreActivity.java
@@ -6,16 +6,28 @@ import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TimePicker;
 
 /**
  * Created by Bart on 2015-03-07.
  */
 public class OffreActivity extends Fragment{
-View rootview;
+    View rootview;
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         rootview = inflater.inflate(R.layout.offre_layout,container,false);
+
+        ((TimePicker)rootview.findViewById(R.id.etBetweenStart)).setIs24HourView(true);
+
+        Button submitOffer = (Button)(rootview.findViewById(R.id.btnSubmitOffer));
+        submitOffer.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
         return rootview;
     }
 }
