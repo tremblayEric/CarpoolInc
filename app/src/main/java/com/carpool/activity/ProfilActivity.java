2634d436477e024a6efebef095e4b37d01a825d3
akouemo Jeanne <jdarisca1@gmail.com>
2015-03-17 16:48:29 -0400

    ajout interface pour profil

--- app/src/main/java/com/carpool/activity/ProfilActivity.java
+++ app/src/main/java/com/carpool/activity/ProfilActivity.java
@@ -7,6 +7,9 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.parse.ParseUser;
+import android.util.Log;
+
 /**
  * Created by Bart on 2015-03-07.
  */
@@ -16,6 +19,31 @@ View rootview;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         rootview = inflater.inflate(R.layout.profil_layout,container,false);
+
+        // Ici je met mon code
+
+        ParseUser currentUser = ParseUser.getCurrentUser();
+
+        if (currentUser != null)
+        {
+            Log.d("trouve", "un utilisateur a ete trouve" + currentUser.getObjectId());
+        }
+        else
+        {
+            Log.d("faux", "un utilisateur na pas ete trouve");
+        }
+
+
+
+
+
+
         return rootview;
+
+
+
+
+
+
     }
 }
