1075c00229d196783f4fdc5c8c7cb726dd64fc3e
eric tremblay <tremblay_eric@live.ca>
2015-03-19 19:56:39 -0400

    ajout de la classe et du layout pour le google de offre

---
+++ app/src/main/java/com/carpool/activity/OffreGooglePlay.java
@@ -0,0 +1,10 @@
+package com.carpool.activity;
+
+/**
+ * cette classe reprend le fonctionnement de la classe adresse, mais dans le contexte de l'offre au
+ * lieu de la recherche
+ * les deux devrait être factorisées et fusionnées en une seule classe, mais je n'ai pas le temps
+ * lors de la phase 1. Navré pour le 'big pile of mud'
+ */
+public class OffreGooglePlay {
+}
