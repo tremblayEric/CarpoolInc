f6b6ba23a7f6e851643089dca78d3a014bcbe4e4
eric tremblay <tremblay_eric@live.ca>
2015-03-21 19:11:54 -0400

    le code qui recherche les offres acceptable est integrer

--- app/src/main/java/com/carpool/activity/RechercheActivity.java
+++ app/src/main/java/com/carpool/activity/RechercheActivity.java
@@ -1,6 +1,7 @@
 package com.carpool.activity;
 
 import android.content.Context;
+import android.location.Location;
 import android.os.Handler;
 import android.os.Message;
 import android.support.v4.app.Fragment;
@@ -14,6 +15,7 @@ import android.widget.ArrayAdapter;
 import android.widget.AutoCompleteTextView;
 import android.widget.Button;
 import android.widget.DatePicker;
+import android.widget.ExpandableListView;
 import android.widget.Filter;
 import android.widget.Filterable;
 import android.widget.NumberPicker;
@@ -24,10 +26,15 @@ import com.carpool.utils.PlaceAPI;
 import com.carpool.model.Offre;
 import com.carpool.model.Position;
 import com.carpool.model.Trajet;
+import com.parse.FindCallback;
+import com.parse.ParseException;
+import com.parse.ParseQuery;
 import com.parse.ParseUser;
 
 import java.util.ArrayList;
 import java.util.Calendar;
+import java.util.Date;
+import java.util.HashMap;
 import java.util.LinkedHashSet;
 import java.util.List;
 
@@ -61,6 +68,7 @@ public class RechercheActivity extends Fragment {
                 GeocodingLocation locationAddress = new GeocodingLocation();
                 locationAddress.getAddressFromLocation(address,
                         getActivity().getApplicationContext(), new GeocoderHandler());
+
             }
         });
         return rootview;
@@ -138,4 +146,116 @@ public class RechercheActivity extends Fragment {
             return filter;
         }
     }
+
+    private void getOffresFromDataBase(){
+
+        final ExpandableListView listView = (ExpandableListView) rootview.findViewById(R.id.lvResultSearch);
+
+        ParseQuery<Offre> query = ParseQuery.getQuery("Offre");
+        query.findInBackground(
+                new FindCallback<Offre>() {
+                    @Override
+                    public void done(List<Offre> offres, ParseException e) {
+                        if (e == null) {
+
+                            List<Offre> offresAcceptables = getOffreCorrespondantes(offres);
+                            MyResultSearchListAdapter adapter = new MyResultSearchListAdapter(getActivity(), offresAcceptables);
+                            listView.setAdapter(adapter);
+                        } else {
+                            /*exception*/
+                        }
+                    }
+                });
+    }
+
+    private List<Offre> getOffreCorrespondantes(List<Offre> offres){
+        /*
+        Calendar cal = Calendar.getInstance();
+        DatePicker dateDepart = (DatePicker)rootview.findViewById(R.id.etDate);
+        cal.set(dateDepart.getYear(), dateDepart.getMonth(), dateDepart.getDayOfMonth());
+
+        offre.setDepart(cal.getTime());
+
+        TimePicker timePicker = (TimePicker)rootview.findViewById(R.id.etBetweenStart);
+        cal.set(Calendar.HOUR, timePicker.getCurrentHour());
+        cal.set(Calendar.MINUTE, timePicker.getCurrentMinute());
+        */
+
+        List<Offre> lesOffres = offres;
+        List<Offre> offresAcceptables = new ArrayList<Offre>();
+
+        DatePicker datePicker = (DatePicker) getActivity().findViewById(R.id.dateRecherche);
+        TimePicker tempsDepart = (TimePicker) getActivity().findViewById(R.id.tempsDepartRecherche);
+        List<HashMap<String,String>> list = (List<HashMap<String,String>>)getArguments().get("ListFromMap");
+
+        Location locationSouhaitable = new Location("locationA");
+
+        locationSouhaitable.setLatitude(Double.parseDouble(((HashMap<String, String>)list.get(0)).get("lat")));
+        locationSouhaitable.setLongitude(Double.parseDouble(((HashMap<String, String>)list.get(0)).get("lng")));
+
+
+        for (int i = 0 ; i < lesOffres.size();++i){
+            Offre uneOffre = lesOffres.get(i);
+
+            Date dateDuDepart = uneOffre.getDepart();
+            long heureDepartAuPlusTot = uneOffre.getHeureDebut().getHours();
+            long heureDepartAuPlusTard = uneOffre.getHeureFin().getHours();
+
+
+            long diffTemps = dateDuDepart.getTime() - getDateFromDatePicker(datePicker).getTime();
+            int jourDeDifference = (int) diffTemps / (1000 * 60 * 60 * 24);
+
+
+            int heureSouhaitable = tempsDepart.getCurrentHour();
+            boolean heureSouhaitableConcordeAvecOffre = ((heureDepartAuPlusTot <= heureSouhaitable ) && (heureDepartAuPlusTard >= heureSouhaitable));
+
+            if(jourDeDifference == 0 && heureSouhaitableConcordeAvecOffre){
+
+                try {
+                    uneOffre.getTrajet().fetchIfNeeded();
+                } catch (ParseException e) {
+                    e.printStackTrace();
+                }
+                Trajet trajetResultat = uneOffre.getTrajet();
+                try {
+                    trajetResultat.getPositionDepart().fetchIfNeeded();
+                    trajetResultat.getPositionArrive().fetchIfNeeded();
+                } catch (ParseException e1) {
+                    e1.printStackTrace();
+                }
+
+                Location locationOfferte = new Location("locationA");
+
+                locationOfferte.setLatitude(trajetResultat.getPositionDepart().getLatitude());
+                locationOfferte.setLongitude(trajetResultat.getPositionDepart().getLongitude());
+
+                double distance = locationSouhaitable.distanceTo(locationOfferte) / 1000;
+                if (distance <= 20) {
+
+                    locationSouhaitable.setLatitude(Double.parseDouble(((HashMap<String, String>) list.get(1)).get("lat")));
+                    locationSouhaitable.setLongitude(Double.parseDouble(((HashMap<String, String>) list.get(1)).get("lng")));
+
+                    locationOfferte.setLatitude(trajetResultat.getPositionArrive().getLatitude());
+                    locationOfferte.setLongitude(trajetResultat.getPositionArrive().getLongitude());
+                    distance = locationSouhaitable.distanceTo(locationOfferte) / 1000;
+                    if (distance <= 20) {
+                        offresAcceptables.add(lesOffres.get(i));
+                    }
+                }
+            }
+        }
+        return offresAcceptables;
+    }
+
+    public static Date getDateFromDatePicker(DatePicker datePicker){
+        int day = datePicker.getDayOfMonth();
+        int month = datePicker.getMonth();
+        int year =  datePicker.getYear();
+
+        Calendar calendar = Calendar.getInstance();
+        calendar.set(year, month, day);
+
+        return calendar.getTime();
+    }
+
 }
