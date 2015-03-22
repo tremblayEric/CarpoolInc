db54c27091217762c4ed1315bcb4230d68f54c45
eric tremblay <tremblay_eric@live.ca>
2015-03-22 19:05:01 -0400

    suppression de code mort, de code en commenaire et de commentaire inutile

--- app/src/main/java/com/carpool/activity/ResultatRechercheActivity.java
+++ app/src/main/java/com/carpool/activity/ResultatRechercheActivity.java
@@ -1,44 +1,24 @@
 package com.carpool.activity;
 
-import android.location.Location;
 import android.support.v4.app.Fragment;
 import android.support.annotation.Nullable;
 import android.os.Bundle;
 import android.view.LayoutInflater;
-import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.DatePicker;
 import android.widget.ExpandableListView;
-import android.widget.TimePicker;
-
 import com.carpool.model.Offre;
-import com.carpool.model.Position;
-import com.carpool.model.Trajet;
-import com.parse.FindCallback;
-import com.parse.ParseException;
-import com.parse.ParseQuery;
-
-import java.util.ArrayList;
-import java.util.Calendar;
-import java.util.Date;
-import java.util.HashMap;
-import java.util.Iterator;
-import java.util.LinkedHashSet;
 import java.util.List;
-
 
 public class ResultatRechercheActivity extends Fragment {
 
     View rootview = null;
-    final ArrayList<Offre> listOffers = new ArrayList<Offre>();
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
     }
-
 
     @Nullable
     @Override
@@ -72,5 +52,4 @@ public class ResultatRechercheActivity extends Fragment {
 
         return super.onOptionsItemSelected(item);
     }
-
 }
