9355a729f62a2bd8d3e0b5a8255257b6d563f391
Gaëlle Claude <oceane3088@gmail.com>
2015-03-22 14:09:17 -0400

    Déconnexion réglé + redirection vers la page de recherche

--- app/src/main/java/com/carpool/activity/LoginActivity.java
+++ app/src/main/java/com/carpool/activity/LoginActivity.java
@@ -18,6 +18,8 @@ import android.os.Bundle;
 import android.provider.ContactsContract;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
 import android.support.v7.app.ActionBarActivity;
 import android.support.v7.widget.Toolbar;
 import android.text.TextUtils;
@@ -338,14 +340,11 @@ public class LoginActivity extends ActionBarActivity {
                 texte.setText("");
                 Log.d("utilis", utilis.getObjectId());
 
-
-
                 //showProgress(false);
-                Intent newActivity  = new Intent(LoginActivity.this, Adresse.class); //Adresse
-
 
                 Log.d("trouve", "utilisateur touve");
 
+                Intent newActivity  = new Intent(LoginActivity.this, AccueilActivity.class); //Accueil
                 startActivity(newActivity);
 
                 finish();
