67a094984889bf3591057a021f136142acdbd400
eric tremblay <tremblay_eric@live.ca>
2015-04-05 13:39:29 -0400

    ajout de code ramsse sur le net pour tracer la route entre deux position. Les routes sont presentemnet codees en dure et il y a un bogue lors du tracage dans la methode onPostExecute avec une liste en parametre. La list des route est null et ca provoque un null pointer exception

--- app/src/main/java/com/carpool/ui/fragments/RechercheResultatFragment.java
+++ app/src/main/java/com/carpool/ui/fragments/RechercheResultatFragment.java
@@ -1,6 +1,7 @@
 package com.carpool.ui.fragments;
 
 import android.graphics.Color;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
@@ -15,12 +16,27 @@ import android.widget.ExpandableListView;
 import com.carpool.R;
 import com.carpool.model.Offre;
 import com.carpool.ui.activities.RechercheResultatActivity;
+import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.carpool.utils.*;
+import com.google.android.gms.maps.model.PolylineOptions;
 
+import org.apache.http.HttpConnection;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 public class RechercheResultatFragment extends Fragment {
@@ -30,6 +46,13 @@ public class RechercheResultatFragment extends Fragment {
     private static final String KEY_POSITION = "position";
     View result;
     int position_;
+
+
+    private static final LatLng LOWER_MANHATTAN = new LatLng(40.722543,
+            -73.998585);
+    private static final LatLng BROOKLYN_BRIDGE = new LatLng(40.7057, -73.9964);
+    private static final LatLng WALL_STREET = new LatLng(40.7064, -74.0094);
+
 
     public static RechercheResultatFragment newInstance(int position) {
         RechercheResultatFragment frag = new RechercheResultatFragment();
@@ -70,7 +93,10 @@ public class RechercheResultatFragment extends Fragment {
                 return (result);
 
             case 1:
+
                 result = inflater.inflate(R.layout.google_card_fragment, null);
+
+                //result = inflater.inflate(R.layout.google_card_fragment, null);
 
                 return (result);
 
@@ -78,6 +104,101 @@ public class RechercheResultatFragment extends Fragment {
         return null;
     }
 
+    private String getMapsApiDirectionsUrl() {
+        String waypoints = "waypoints=optimize:true|"
+                + LOWER_MANHATTAN.latitude + "," + LOWER_MANHATTAN.longitude
+                + "|" + "|" + BROOKLYN_BRIDGE.latitude + ","
+                + BROOKLYN_BRIDGE.longitude + "|" + WALL_STREET.latitude + ","
+                + WALL_STREET.longitude;
+
+        String sensor = "sensor=false";
+        String params = waypoints + "&" + sensor;
+        String output = "json";
+        String url = "https://maps.googleapis.com/maps/api/directions/"
+                + output + "?" + params;
+        return url;
+    }
+
+    private void addMarkers() {
+        if (map != null) {
+            map.addMarker(new MarkerOptions().position(BROOKLYN_BRIDGE)
+                    .title("First Point"));
+            map.addMarker(new MarkerOptions().position(LOWER_MANHATTAN)
+                    .title("Second Point"));
+            map.addMarker(new MarkerOptions().position(WALL_STREET)
+                    .title("Third Point"));
+        }
+    }
+
+    private class ReadTask extends AsyncTask<String, Void, String> {
+        @Override
+        protected String doInBackground(String... url) {
+            String data = "";
+            try {
+                HttpConnection http = new HttpConnection();
+                data = http.readUrl(url[0]);
+            } catch (Exception e) {
+                Log.d("Background Task", e.toString());
+            }
+            return data;
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+            super.onPostExecute(result);
+            new ParserTask().execute(result);
+        }
+    }
+
+    private class ParserTask extends
+            AsyncTask<String, Integer, List<List<HashMap<String, String>>>> {
+
+        @Override
+        protected List<List<HashMap<String, String>>> doInBackground(
+                String... jsonData) {
+
+            JSONObject jObject;
+            List<List<HashMap<String, String>>> routes = null;
+
+            try {
+                jObject = new JSONObject(jsonData[0]);
+                PathJSONParser parser = new PathJSONParser();
+                routes = parser.parse(jObject);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            return routes;
+        }
+
+        @Override
+        protected void onPostExecute(List<List<HashMap<String, String>>> routes) {
+            ArrayList<LatLng> points = null;
+            PolylineOptions polyLineOptions = null;
+
+            // traversing through routes
+            for (int i = 0; i < routes.size(); i++) {
+                points = new ArrayList<LatLng>();
+                polyLineOptions = new PolylineOptions();
+                List<HashMap<String, String>> path = routes.get(i);
+
+                for (int j = 0; j < path.size(); j++) {
+                    HashMap<String, String> point = path.get(j);
+
+                    double lat = Double.parseDouble(point.get("lat"));
+                    double lng = Double.parseDouble(point.get("lng"));
+                    LatLng position = new LatLng(lat, lng);
+
+                    points.add(position);
+                }
+
+                polyLineOptions.addAll(points);
+                polyLineOptions.width(2);
+                polyLineOptions.color(Color.BLUE);
+            }
+
+            map.addPolyline(polyLineOptions);
+        }
+    }
 
     @Override
     public void onViewCreated(View view, Bundle savedInstanceState) {
@@ -150,10 +271,139 @@ public class RechercheResultatFragment extends Fragment {
 
                 if (map == null) {
                 map = fragment.getMap();
-                map.addMarker(new MarkerOptions().position(new LatLng(12, -12)));
+               // map.addMarker(new MarkerOptions().position(new LatLng(12, -12)));
+                    MarkerOptions options = new MarkerOptions();
+                    options.position(LOWER_MANHATTAN);
+                    options.position(BROOKLYN_BRIDGE);
+                    options.position(WALL_STREET);
+                    map.addMarker(options);
+                    String url = getMapsApiDirectionsUrl();
+                    ReadTask downloadTask = new ReadTask();
+                    downloadTask.execute(url);
+
+                    map.moveCamera(CameraUpdateFactory.newLatLngZoom(BROOKLYN_BRIDGE,
+                            13));
+                    addMarkers();
                 int i = 0;//test pour provoquer l'addition des changement à la branche
                 break;
             }
+        }
+    }
+
+
+
+    public class HttpConnection {
+        public String readUrl(String mapsApiDirectionsUrl) throws IOException {
+            String data = "";
+            InputStream iStream = null;
+            HttpURLConnection urlConnection = null;
+            try {
+                URL url = new URL(mapsApiDirectionsUrl);
+                urlConnection = (HttpURLConnection) url.openConnection();
+                urlConnection.connect();
+                iStream = urlConnection.getInputStream();
+                BufferedReader br = new BufferedReader(new InputStreamReader(
+                        iStream));
+                StringBuffer sb = new StringBuffer();
+                String line = "";
+                while ((line = br.readLine()) != null) {
+                    sb.append(line);
+                }
+                data = sb.toString();
+                br.close();
+            } catch (Exception e) {
+                Log.d("Exception while reading url", e.toString());
+            } finally {
+                iStream.close();
+                urlConnection.disconnect();
+            }
+            return data;
+        }
+
+    }
+
+    public class PathJSONParser {
+
+        public List<List<HashMap<String, String>>> parse(JSONObject jObject) {
+            List<List<HashMap<String, String>>> routes = new ArrayList<List<HashMap<String, String>>>();
+            JSONArray jRoutes = null;
+            JSONArray jLegs = null;
+            JSONArray jSteps = null;
+            try {
+                jRoutes = jObject.getJSONArray("routes");
+                /** Traversing all routes */
+                for (int i = 0; i < jRoutes.length(); i++) {
+                    jLegs = ((JSONObject) jRoutes.get(i)).getJSONArray("legs");
+                    List<HashMap<String, String>> path = new ArrayList<HashMap<String, String>>();
+
+                    /** Traversing all legs */
+                    for (int j = 0; j < jLegs.length(); j++) {
+                        jSteps = ((JSONObject) jLegs.get(j)).getJSONArray("steps");
+
+                        /** Traversing all steps */
+                        for (int k = 0; k < jSteps.length(); k++) {
+                            String polyline = "";
+                            polyline = (String) ((JSONObject) ((JSONObject) jSteps
+                                    .get(k)).get("polyline")).get("points");
+                            List<LatLng> list = decodePoly(polyline);
+
+                            /** Traversing all points */
+                            for (int l = 0; l < list.size(); l++) {
+                                HashMap<String, String> hm = new HashMap<String, String>();
+                                hm.put("lat",
+                                        Double.toString(((LatLng) list.get(l)).latitude));
+                                hm.put("lng",
+                                        Double.toString(((LatLng) list.get(l)).longitude));
+                                path.add(hm);
+                            }
+                        }
+                        routes.add(path);
+                    }
+                }
+
+            } catch (JSONException e) {
+                e.printStackTrace();
+            } catch (Exception e) {
+            }
+            return routes;
+        }
+
+        /**
+         * Method Courtesy :
+         * jeffreysambells.com/2010/05/27
+         * /decoding-polylines-from-google-maps-direction-api-with-java
+         * */
+        private List<LatLng> decodePoly(String encoded) {
+
+            List<LatLng> poly = new ArrayList<LatLng>();
+            int index = 0, len = encoded.length();
+            int lat = 0, lng = 0;
+
+            while (index < len) {
+                int b, shift = 0, result = 0;
+                do {
+                    b = encoded.charAt(index++) - 63;
+                    result |= (b & 0x1f) << shift;
+                    shift += 5;
+                } while (b >= 0x20);
+                int dlat = ((result & 1) != 0 ? ~(result >> 1) : (result >> 1));
+                lat += dlat;
+
+                shift = 0;
+                result = 0;
+                do {
+                    b = encoded.charAt(index++) - 63;
+                    result |= (b & 0x1f) << shift;
+                    shift += 5;
+                } while (b >= 0x20);
+                int dlng = ((result & 1) != 0 ? ~(result >> 1) : (result >> 1));
+                lng += dlng;
+
+                LatLng p = new LatLng((((double) lat / 1E5)),
+                        (((double) lng / 1E5)));
+                poly.add(p);
+            }
+            return poly;
         }
     }
 }
